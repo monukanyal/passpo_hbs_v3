38e472b9097ba75570ef92d4003dbb5b9f777de4
ip-d <monu_kanyal@esferasoft.com>
2018-04-17 15:01:16 +0530

    3:02pm

--- routes/user.js
+++ routes/user.js
@@ -17,14 +17,9 @@ var opts = {
   key: 'AIzaSyDnjiB1yHIkT3BDVTpf80LqC3fMZ8_ygIU'
 };
 
-
 /* var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url ="mongodb://Esfera:esfera456@ds133547.mlab.com:33547/esferasoft"; */
-
-
-/* GET user dashbard. */
-
 
 const getinfo = (req, res) => {
     //var arr="[{title: 'Happy Forever Alone Day',file: 'Happy Forever Alone Day (Forever Alone Song)',howl: null},{title:'In The End',file:'In The End _Official Video_ - Linkin Park',howl:null},{title:'Swag Se Swagat Song',file:'Swag Se Swagat Song',howl:null}]";
@@ -52,7 +47,7 @@ const getinfo = (req, res) => {
 				  if (!error && response.statusCode == 200) {
 				    var $ = cheerio.load(html);
 				    var maindata= $('.view-content').children();
-				   console.log(parseInt(maindata.length));
+
 				    $('.view-content').children().each(function(j,element){
 				    	  var newsobj={};
 				    	  var details=$(element).html($('.detail h3').attr('title')).text();
@@ -72,72 +67,13 @@ const getinfo = (req, res) => {
 			    	
 			    }
 			});  
-	
-		//
 	}).catch((err)=>{
 		console.log(err);
 		res.render('dashboard'	,{message:req.flash('success'),Name:req.session.passport.user.Name,playerlist:'',newsdata:news,layout: 'layout_dash.hbs'});
 	});
 };
-router.get('/',ensureLoggedIn,getinfo);
-/*router.get('/', ensureLoggedIn, function(req, res, next) 
-{
-	//var arr="[{title: 'Happy Forever Alone Day',file: 'Happy Forever Alone Day (Forever Alone Song)',howl: null},{title:'In The End',file:'In The End _Official Video_ - Linkin Park',howl:null},{title:'Swag Se Swagat Song',file:'Swag Se Swagat Song',howl:null}]";
-	Song.find({}).sort([['createdAt', 'descending']]).exec().then((results)=>{
-	   var mainarr=[];
-	   var i=0;
-	 	var news=[];
-		async.each(results, function(result, callback) {
-			    var myObj = {};
-			       i=i+1;
-					myObj.title = result.filepath;
-					myObj.file = result.filepath;
-					myObj.howl = null;
-					mainarr.push(myObj);
-					if(i==parseInt(results.length))
-					{
-					  callback(mainarr,null);
-					}
-			}, function(result,err) {
-			 
-			    if( err ) {
-			      console.log('failed to process');
-			    } else {
-			    	 request('https://www.indiatoday.in/india', function (error, response, html) {
-				  if (!error && response.statusCode == 200) {
-				    var $ = cheerio.load(html);
-				    var maindata= $('.view-content').children();
-				   console.log(parseInt(maindata.length));
-				    $('.view-content').children().each(function(j,element){
-				    	  var newsobj={};
-				    	  var details=$(element).html($('.detail h3').attr('title')).text();
-				    	//console.log(i);
-				    	//console.log('onediv length:'+$(element).children().length);
-				    	//console.log('onediv:'+$(element).html());
-				    	newsobj.picurl=$(element).html($('.pic img').attr('src')).text();
-				    	newsobj.desc=details;
-				    	news.push(newsobj);
-				    	//console.log('j='+j+':'+$(element).html($('img').attr('src')).text());
-				    	//console.log('j='+j+':'+$(element).html($('.detail').text()).text());
-				    });
-				    //console.log(news);
-			      	res.render('dashboard',{message:req.flash('success'),Name:req.session.passport.user.Name,newsdata:news,playerlist:JSON.stringify(result),layout: 'layout_dash.hbs'});
-				  }
-				});
-			    	
-			    }
-			});  
-	
-		//
-	}).catch((err)=>{
-		console.log(err);
-		res.render('dashboard'	,{message:req.flash('success'),Name:req.session.passport.user.Name,playerlist:'',newsdata:news,layout: 'layout_dash.hbs'});
-	});
-	//console.log('session email:'+req.session.passport.user.Name);
-   
 
-	
-}); */
+router.get('/',ensureLoggedIn,getinfo);  //dashboard
 
 router.get('/profile', ensureLoggedIn, function(req, res, next) {
     User.find({_id:req.session.passport.user._id}).exec(function(err,results){
@@ -151,6 +87,7 @@ router.get('/profile', ensureLoggedIn, function(req, res, next) {
     
   });
 }); 
+
 router.post('/search', ensureLoggedIn, function(req, res, next) 
 {
 	var searchterm=req.body.query;
